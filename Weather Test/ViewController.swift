7fda4797d81a682ef86aec942aed92d524eaa6f5
Bryan Mazariegos <heat200@bryans-macbook-pro-4.local>
2017-06-12 23:53:06 -0400

    -Finished

--- Weather Test/ViewController.swift
+++ Weather Test/ViewController.swift
@@ -8,18 +8,109 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var weatherBackground:UIImageView!
+    @IBOutlet var zipcodeTextField:UITextField!
+    @IBOutlet var currentTempLabel:UILabel!
+    @IBOutlet var highTempLabel:UILabel!
+    @IBOutlet var lowTempLabel:UILabel!
+    @IBOutlet var unitSelector: UISegmentedControl!
+    
+    var zipcode = "33193"
+    var units = "imperial"
+    var background = ""
+    var tempLow:CGFloat = 0
+    var tempCurrent:CGFloat = 0
+    var tempHigh:CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        zipcodeTextField.text = zipcode
+        updateData()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            if self.view.point(inside: touch.location(in: self.view), with: nil) {
+                if zipcodeTextField!.isFirstResponder {
+                    zipcodeTextField.resignFirstResponder()
+                    if zipcodeTextField.text?.characters.count != 5 {
+                        zipcodeTextField.text = zipcode
+                    }
+                    updateData()
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateUI() {
+        weatherBackground.image = UIImage(named: background)
+        currentTempLabel.text = "\(tempCurrent)°"
+        highTempLabel.text = "\(tempHigh)°"
+        lowTempLabel.text = "\(tempLow)°"
+        
+        if units == "imperial" {
+            currentTempLabel.text?.append(" F")
+            highTempLabel.text?.append(" F")
+            lowTempLabel.text?.append(" F")
+        } else {
+            currentTempLabel.text?.append(" C")
+            highTempLabel.text?.append(" C")
+            lowTempLabel.text?.append(" C")
+        }
+    }
+    
+    @IBAction func updateData() {
+        DispatchQueue.global().async {
+            let data = self.pullData()
+            DispatchQueue.main.async {
+                do {
+                    let dictionary = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves)
+                    let mainDict = (dictionary as AnyObject).object(forKey: "weather") as! [AnyObject]
+                    self.background = mainDict[0].object(forKey: "main") as! String
+                    
+                    let tempDict = (dictionary as AnyObject).object(forKey: "main")!
+                    self.tempCurrent = ((tempDict as AnyObject).value(forKey: "temp") as! CGFloat)
+                    self.tempHigh = ((tempDict as AnyObject).value(forKey: "temp_max") as! CGFloat)
+                    self.tempLow = ((tempDict as AnyObject).value(forKey: "temp_min") as! CGFloat)
+                    self.updateUI()
+                } catch {
+                    print("Could not parse JSON: \(error)")
+                }
+            }
+        }
     }
 
-
+    func pullData() -> Data? {
+        if zipcodeTextField.text?.characters.count == 5 {
+            zipcode = zipcodeTextField.text!
+        }
+        
+        if unitSelector != nil {
+            if unitSelector.selectedSegmentIndex == 0 {
+                units = "imperial"
+            } else {
+                units = "metric"
+            }
+        }
+        
+        let urlString = "http://api.openweathermap.org/data/2.5/weather?zip=\(zipcode),us&units=\(units)&APPID=626b4222b1f655ed8c4ebdd6017e9848"
+        let returnData = try? Data(contentsOf: URL(string: urlString)!)
+        
+        return returnData
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
